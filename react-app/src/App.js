fa93d59495fe51cac7631907381197391a1b1ba8
julia-richards <julia.m.richards@gmail.com>
2021-01-10 16:58:10 -0700

    adds switch so only 1 matching route at time

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import User from "./components/User";
 import PlanForm from "./components/PlanForm";
-import Map from "./components/RoutePlan"
+import RouteForm from "./components/RouteForm"
+import RoutePage from "./components/RoutePage"
+import Layout from "./components/Layout"
 import { authenticate } from "./services/auth";
 
 
@@ -32,42 +34,52 @@ function App() {
   return (
 		<BrowserRouter>
 			<NavBar setAuthenticated={setAuthenticated} />
-			<Route path="/login" exact={true}>
-				<LoginForm
+			<Switch>
+				<Route path="/login" exact={true}>
+					<LoginForm
+						authenticated={authenticated}
+						setAuthenticated={setAuthenticated}
+					/>
+				</Route>
+				<Route path="/sign-up" exact={true}>
+					<SignUpForm
+						authenticated={authenticated}
+						setAuthenticated={setAuthenticated}
+					/>
+				</Route>
+				<ProtectedRoute
+					path="/users/:userId"
+					exact={true}
 					authenticated={authenticated}
-					setAuthenticated={setAuthenticated}
-				/>
-			</Route>
-			<Route path="/sign-up" exact={true}>
-				<SignUpForm
+				>
+					<User />
+				</ProtectedRoute>
+				<ProtectedRoute path="/" exact={true} authenticated={authenticated}>
+					<Layout />
+					<h1 style={{position: 'absolute', top: 10, right: 100 }}>Profile</h1>
+				</ProtectedRoute>
+				<ProtectedRoute
+					path="/plans/new"
+					exact={true}
 					authenticated={authenticated}
-					setAuthenticated={setAuthenticated}
-				/>
-			</Route>
-			<ProtectedRoute
-				path="/users/:userId"
-				exact={true}
-				authenticated={authenticated}
-			>
-				<User />
-			</ProtectedRoute>
-			<ProtectedRoute path="/" exact={true} authenticated={authenticated}>
-				<h1>My Home Page</h1>
-			</ProtectedRoute>
-			<ProtectedRoute
-				path="/plans/new"
-				exact={true}
-				authenticated={authenticated}
-			>
-				<PlanForm />
-			</ProtectedRoute>
-      <ProtectedRoute
-				path="/routes/new"
-				exact={true}
-				authenticated={authenticated}
-			>
-				<Map />
-			</ProtectedRoute>
+				>
+					<PlanForm />
+				</ProtectedRoute>
+				<ProtectedRoute
+					path="/routes/new"
+					authenticated={authenticated}
+					exact
+				>
+					<RouteForm />
+				</ProtectedRoute>
+				<ProtectedRoute
+					path="/routes/:routeId"
+					authenticated={authenticated}
+					exact
+				>
+					<RoutePage />
+				</ProtectedRoute>
+			</Switch>
 		</BrowserRouter>
 	);
 }
