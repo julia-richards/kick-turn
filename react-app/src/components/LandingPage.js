5179d59f242c2baa003d7e28d477c273a2a17bbe
julia-richards <julia.m.richards@gmail.com>
2021-01-13 22:14:35 -0700

    reworks landing page to closer to final state

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -10,22 +10,34 @@ import {
 
 
 const LandingPage = () => (
+	<div className='landing'>
+		{/* <img className='landing__background' src="/images/background.svg"/> */}
 	<Layout style={{ backgroundColor: "gray" }}>
 		<Seo title={"Kick Turn"} />
 		<div className="landing__elements">
 		<div className="landing__elements-welcome">
+			<img src="/images/fadedMap.svg"/>
 			<h1>Plan a Tour</h1>
 			<p>Assess avalanche conditions</p>
-			<p>Plan your route</p>
+			<p>Map your route</p>
 			<p>Collaborate with ski partners</p>
-			<button className="welcome__signup-button">Get Started</button>
+			<button className="welcome__signup-button">Go Skiing</button>
 		</div>
 		<div className="landing__elements-weather">
+			<div className="landing__elements-weather__snow">
 			<FontAwesomeIcon icon={faSnowflake} />
+			<p className="landing__elements-weather__value">2"</p>
+			<p className="landing__elements-weath__label">24 hrs</p>
+			</div>
+			<div className="landing__elements-weather__temp">
 			<FontAwesomeIcon icon={faTemperatureLow} />
+			<p className="landing__elements-weather__value">27°F</p>
+			<p className="landing__elements-weath__label">Current Temp</p>
+			</div>
 		</div>
 		</div>
 	</Layout>
+	</div>
 );
 
 export default LandingPage;
