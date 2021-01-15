0e99ea66dcc48282c89d1358687056178959fddb
julia-richards <julia.m.richards@gmail.com>
2021-01-15 13:55:18 -0700

    moves images to src and adjusts imports accordingly

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -4,39 +4,41 @@ import Seo from "./Seo";
 import "../styles/LandingPage.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faSnowflake,
-	faTemperatureLow,
+  faSnowflake,
+  faTemperatureLow,
 } from "@fortawesome/free-solid-svg-icons";
 
+import background from "../images/background.svg";
+import fadedMap from "../images/fadedMap.svg";
 
 const LandingPage = () => (
-	<div className="landing">
-		<Layout>
-			<Seo title={"Kick Turn"} />
-			<div className="landing__elements">
-				<div className="landing__elements-welcome">
-					<img src="/images/fadedMap.svg" />
-					<h1>Plan a Tour</h1>
-					<p>Assess avalanche conditions</p>
-					<p>Map your route</p>
-					<p>Collaborate with ski partners</p>
-					<button className="welcome__signup-button">Go Skiing</button>
-				</div>
-				<div className="landing__elements-weather">
-					<div className="landing__elements-weather__snow">
-						<FontAwesomeIcon icon={faSnowflake} />
-						<p className="landing__elements-weather__value">2"</p>
-						<p className="landing__elements-weath__label">24 hrs</p>
-					</div>
-					<div className="landing__elements-weather__temp">
-						<FontAwesomeIcon icon={faTemperatureLow} />
-						<p className="landing__elements-weather__value">27°F</p>
-						<p className="landing__elements-weath__label">Current Temp</p>
-					</div>
-				</div>
-			</div>
-		</Layout>
-	</div>
+  <div className="landing" style={{ backgroundImage: `url(${background})` }}>
+    <Layout>
+      <Seo title={"Kick Turn"} />
+      <div className="landing__elements">
+        <div className="landing__elements-welcome">
+          <img src={fadedMap} />
+          <h1>Plan a Tour</h1>
+          <p>Assess avalanche conditions</p>
+          <p>Map your route</p>
+          <p>Collaborate with ski partners</p>
+          <button className="welcome__signup-button">Go Skiing</button>
+        </div>
+        <div className="landing__elements-weather">
+          <div className="landing__elements-weather__snow">
+            <FontAwesomeIcon icon={faSnowflake} />
+            <p className="landing__elements-weather__value">2"</p>
+            <p className="landing__elements-weath__label">24 hrs</p>
+          </div>
+          <div className="landing__elements-weather__temp">
+            <FontAwesomeIcon icon={faTemperatureLow} />
+            <p className="landing__elements-weather__value">27°F</p>
+            <p className="landing__elements-weath__label">Current Temp</p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  </div>
 );
 
 export default LandingPage;
