0de8ef1da5b440abe0ed958865307a2207226116
julia-richards <julia.m.richards@gmail.com>
2021-01-15 00:12:21 -0700

    adds row prop

--- react-app/src/components/formComponents/TextArea.js
+++ react-app/src/components/formComponents/TextArea.js
@@ -1,24 +1,33 @@
 import React from "react";
 import { titleize } from "../../utils";
 
-const TextArea = ({ name, label, formValues, setValue, placeholder, hint }) => {
-	const title = label || titleize(name);
+const TextArea = ({
+  name,
+  label,
+  formValues,
+  setValue,
+  placeholder,
+  hint,
+  rows = 3,
+}) => {
+  const title = label || titleize(name);
 
-	return (
-		<div style={{ display: "flex", flexDirection: "column", marginBottom: 10 }}>
-			<label style={{ fontWeight: "bold" }} htmlFor={name}>
-				{title}
-			</label>
+  return (
+    <div style={{ display: "flex", flexDirection: "column", marginBottom: 10 }}>
+      <label style={{ fontWeight: "bold" }} htmlFor={name}>
+        {title}
+      </label>
       {!!hint && <small>{hint}</small>}
-			<textarea
-				name={name}
-				id={name}
-				value={formValues[name]}
-				onChange={(e) => setValue(name, e.target.value)}
-				placeholder={placeholder || `${title}...`}
-			/>
-		</div>
-	);
+      <textarea
+        name={name}
+        id={name}
+        value={formValues[name]}
+        onChange={(e) => setValue(name, e.target.value)}
+        placeholder={placeholder || `${title}...`}
+        rows={rows}
+      />
+    </div>
+  );
 };
 
 export default TextArea;
