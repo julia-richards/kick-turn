e4dd168336ed8a0b34f2e36cdc0132b41471c7cd
julia-richards <julia.m.richards@gmail.com>
2021-01-15 22:06:36 -0700

    applies current prettier

--- react-app/src/hooks/useAuthenticated.js
+++ react-app/src/hooks/useAuthenticated.js
@@ -1,43 +1,52 @@
 import React, {
-	createContext,
-	useEffect,
-	useMemo,
-	useState,
-	useContext,
+  createContext,
+  useEffect,
+  useMemo,
+  useState,
+  useContext,
 } from "react";
 import { authenticate } from "../services/auth";
 let AuthContext;
 const { Provider } = (AuthContext = createContext());
 
 const AuthProvider = (props) => {
-	const [authenticated, setAuthenticated] = useState(false);
-	const [loaded, setLoaded] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
-	useEffect(() => {
-		(async () => {
-			const user = await authenticate();
-			if (!user.errors) {
-				setAuthenticated(true);
-			}
-			setLoaded(true);
-		})();
-	}, []);
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const user = await authenticate();
+        if (!user.errors) {
+          setAuthenticated(true);
+        }
+      } catch (_e) {
+        console.log("not logged in per", _e);
+      }
+    };
+    try {
+      checkAuth();
+    } catch (e) {
+      console.log("ETF");
+    }
+    setLoaded(true);
+  }, []);
 
-	let providedValue = useMemo(() => [authenticated, setAuthenticated], [
-		authenticated,
-		setAuthenticated,
-	]);
+  let providedValue = useMemo(() => [authenticated, setAuthenticated], [
+    authenticated,
+    setAuthenticated,
+  ]);
 
-	if (!loaded) {
-		return <h4 style={{ margin: "4rem" }}>Loading...</h4>;
-	}
+  if (!loaded) {
+    return <h4 style={{ margin: "4rem" }}>Loading...</h4>;
+  }
 
-	return <Provider value={providedValue}>{props.children}</Provider>;
+  return <Provider value={providedValue}>{props.children}</Provider>;
 };
 
 const useAuthenticated = () => {
-	const value = useContext(AuthContext);
-	return value;
+  const value = useContext(AuthContext);
+  return value;
 };
 
 export { AuthProvider, useAuthenticated };
