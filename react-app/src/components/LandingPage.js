9cf38a2f7b87c6a0e4856ae7202e4e6b6327a628
julia-richards <julia.m.richards@gmail.com>
2021-01-17 13:11:50 -0700

    fixes nav bar slide
    this makes it so that Layout can take more props. this is important as
    it ensures that the landing wrapper class, which comes with special
    styling, is the outermost div on the page.
    
    this means that the nav menu works when closing!

--- react-app/src/components/LandingPage.js
+++ react-app/src/components/LandingPage.js
@@ -13,35 +13,33 @@ import background from "../images/background.svg";
 import fadedMap from "../images/fadedMap.svg";
 
 const LandingPage = () => (
-  <div className="landing" style={{ backgroundImage: `url(${background})` }}>
-    <Layout>
-      <Seo title={"Kick Turn"} />
-      <div className="landing__elements">
-        <div className="landing__elements-welcome">
-          <img alt="background map" src={fadedMap} />
-          <h1>Plan a Tour</h1>
-          <p>Assess avalanche conditions</p>
-          <p>Map your route</p>
-          <p>Collaborate with ski partners</p>
-          <Link to="login" className="welcome__signup-link">
-            Go Skiing
-          </Link>
+  <Layout className="landing" style={{ backgroundImage: `url(${background})` }}>
+    <Seo title={"Kick Turn"} />
+    <div className="landing__elements">
+      <div className="landing__elements-welcome">
+        <img alt="background map" src={fadedMap} />
+        <h1>Plan a Tour</h1>
+        <p>Assess avalanche conditions</p>
+        <p>Map your route</p>
+        <p>Collaborate with ski partners</p>
+        <Link to="login" className="welcome__signup-link">
+          Go Skiing
+        </Link>
+      </div>
+      <div className="landing__elements-weather">
+        <div className="landing__elements-weather__snow">
+          <FontAwesomeIcon icon={faSnowflake} />
+          <p className="landing__elements-weather__value">2"</p>
+          <p className="landing__elements-weath__label">24 hrs</p>
         </div>
-        <div className="landing__elements-weather">
-          <div className="landing__elements-weather__snow">
-            <FontAwesomeIcon icon={faSnowflake} />
-            <p className="landing__elements-weather__value">2"</p>
-            <p className="landing__elements-weath__label">24 hrs</p>
-          </div>
-          <div className="landing__elements-weather__temp">
-            <FontAwesomeIcon icon={faTemperatureLow} />
-            <p className="landing__elements-weather__value">27°F</p>
-            <p className="landing__elements-weath__label">Current Temp</p>
-          </div>
+        <div className="landing__elements-weather__temp">
+          <FontAwesomeIcon icon={faTemperatureLow} />
+          <p className="landing__elements-weather__value">27°F</p>
+          <p className="landing__elements-weath__label">Current Temp</p>
         </div>
       </div>
-    </Layout>
-  </div>
+    </div>
+  </Layout>
 );
 
 export default LandingPage;
--- react-app/src/components/Layout.js
+++ react-app/src/components/Layout.js
@@ -2,13 +2,17 @@ import React from "react";
 import NavBar from "./NavBar";
 import "../styles/Layout.css";
 
-const Layout = ({ children }) => (
-	<div id="Layout" className="Layout">
-		<NavBar outerContainerId="Layout" pageWrapId="Layout__main" />
-		<main className="Layout__main" id="Layout__main">
-			{children}
-		</main>
-	</div>
+const Layout = ({ children, className, ...props }) => (
+  <div
+    id="Layout"
+    className={`Layout${!!className ? ` ${className}` : ""}`}
+    {...props}
+  >
+    <NavBar outerContainerId="Layout" pageWrapId="Layout__main" />
+    <main className="Layout__main" id="Layout__main">
+      {children}
+    </main>
+  </div>
 );
 
 export default Layout;
