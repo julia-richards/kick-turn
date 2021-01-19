236e19979b75f0e71253f1f231527f989dfa25e7
julia-richards <julia.m.richards@gmail.com>
2021-01-18 22:13:25 -0700

    renders all tours on my tours

--- react-app/src/components/MyTours/TourPlanItem.js
+++ react-app/src/components/MyTours/TourPlanItem.js
@@ -1,9 +1,25 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import StaticMap from "../Maps/StaticMap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExpandArrowsAlt } from "@fortawesome/free-solid-svg-icons";
 const TourPlanItem = ({ tour }) => {
   return (
     <li className="tour-plan-item">
-      <pre>{JSON.stringify(tour, null, 2)}</pre>
+      <div className="tour-plan-item__text">
+        <h2>{tour.date}</h2>
+        <p>{tour.tour_plan}</p>
+        <Link to={`/plans/${tour.id}`} className="link-button">
+          <FontAwesomeIcon icon={faExpandArrowsAlt} /> View
+        </Link>
+      </div>
+      <div className="tour-plan-item__map">
+        <StaticMap
+          features={tour.route.geo_features}
+          defaultViewport={tour.route.viewport}
+          style={{ height: 300 }}
+        />
+      </div>
     </li>
   );
 };
